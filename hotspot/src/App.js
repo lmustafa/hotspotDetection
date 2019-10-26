6f4d55bdc4897d22be686ca6219f95671883733f
Laiba Mustafa <lmustafa@uoguelph.ca>
2019-10-26 13:25:49 -0400

    first commit

---
+++ hotspot/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+	// state = {
+	// 	characters: [],
+	// }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
+export default App
